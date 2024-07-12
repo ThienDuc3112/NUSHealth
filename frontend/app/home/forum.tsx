148e94180a534d775afcfeedc83c8d4c42612c44
Thien Duc <ntduc4@gmail.com>
2024-07-12 22:01:07 +0700

    fetching forum post

--- frontend/app/home/forum.tsx
+++ frontend/app/home/forum.tsx
@@ -1,14 +1,25 @@
 import { StyleSheet, Text, View } from "react-native";
-import React from "react";
+import React, { useCallback } from "react";
+import { useQuery } from "@tanstack/react-query";
+import { API } from "@/constants/api";
+import { useFocusEffect } from "expo-router";
 
-const Training = () => {
+const Page = () => {
+  const { data, error, isLoading, refetch } = useQuery({
+    queryKey: ["posts"],
+    queryFn: async () => fetch(`${API}/post/all`)
+  })
+
+  useFocusEffect(useCallback(() => { refetch() }, []))
+
   return (
-    <View>
+    <View style={{ flex: 1 }}>
       <Text>Forum</Text>
+      <Text>Data: {data && JSON.stringify(data, null, 2)}</Text>
     </View>
   );
 };
 
-export default Training;
+export default Page;
 
 const styles = StyleSheet.create({});
